6fa6ca1c74c2e5fb31947f2e4a160a65e55e1348
lokesh <165912452+Glokeshvardhan@users.noreply.github.com>
2025-05-23 16:51:07 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 import Contact from "./components/Contact";
-
+import Vacancies from "./components/vacancies";
+import Navbar from './components/Navbar';
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -16,12 +17,16 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="/contact" element={<Contact />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/vacancies" element={<Vacancies />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/#:section" element={<Navigate to="/" />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
