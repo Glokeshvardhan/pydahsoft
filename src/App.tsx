2183d9018705c29828e955f163cc46d7eb9a2e41
lokesh <165912452+Glokeshvardhan@users.noreply.github.com>
2025-05-27 16:29:19 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import Blog from "./pages/Blog";
+import WebDevelopment from "./pages/WebDevelopment";
 import Contact from "./components/Contact";
 import Vacancies from "./components/vacancies";
 import Navbar from './components/Navbar';
+import WhatsAppChat from './components/WhatsAppChat';
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -23,9 +26,12 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/vacancies" element={<Vacancies />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/web-development" element={<WebDevelopment />} />
             <Route path="/#:section" element={<Navigate to="/" />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
+          <WhatsAppChat />
         </div>
       </BrowserRouter>
     </TooltipProvider>
